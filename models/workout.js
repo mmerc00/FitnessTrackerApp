b4eb8df09a667a4d3cadfeab551fc1723be07c40
Melissa Mercado <melissa.a.merc@gmail.com>
2020-08-18 18:14:12 -0400

    added starting dependencies and a few routes as well as psuedo code

---
+++ models/workout.js
@@ -0,0 +1,5 @@
+//list workout
+//variable workout like type, reps and distance duration
+
+let Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(logger("dev"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/custommethods",
+  { useNewUrlParser: true }
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const db = mongojs(databaseUrl, collections);
+db.on("error", (error) => {
+  console.log("Database Error:", error);
+});
+
+// Listen on port 3000
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
