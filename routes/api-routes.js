50d594a730cd183a0bb3a802abeb1b986e6fec2c
Melissa Mercado <melissa.a.merc@gmail.com>
2020-08-19 17:12:32 -0400

    gotchu cam

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,6 +20,35 @@ app.post("/workouts", ({ body }, res) => {
       res.json(err);
     });
 });
+
 //add workout put
+app.put("/workouts", ({ body }, res) => {
+  db.Workout.update(body)
+    .then(({ _id }) =>
+      db.Library.findOneAndUpdate(
+        {},
+        { $push: { workout: _id } },
+        { new: true }
+      )
+    )
+    .then((dbLibrary) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //retrieve workouts? get
+app.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    //populate properties in the book array
+    //populate("librarians")- chain populates with models
+    .populate("workouts")
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
